b526392f7b3523b5a230077dbc8b1c2b79e7a5c7
Anurag Kumar <anurag.kumar@wwt.com>
2022-11-29 13:04:50 -0600

    Added Transaction data for every custoomer

---
+++ src/api/rewardService.js
@@ -0,0 +1,66 @@
+export default function() {
+  return Promise.resolve(
+    [
+        {
+          custid: 1,
+          name: "Customer 1",
+          amount: 120,
+          transactionDate: "11-09-2022"
+        },
+        {
+          custid: 1,
+          name: "Customer 1",
+          amount: 75,
+          transactionDate: "10-19-2022"
+        },
+        {
+          custid: 1,
+          name: "Customer 1",
+          amount: 94,
+          transactionDate: "11-21-2022"
+        },
+        {
+          custid: 1,
+          name: "Customer 1",
+          amount: 10,
+          transactionDate: "11-27-2022"
+        },
+        {
+          custid: 2,
+          name: "Customer 2",
+          amount: 125,
+          transactionDate: "10-20-2022"
+        },
+        {
+          custid: 2,
+          name: "Customer 2",
+          amount: 75,
+          transactionDate: "09-29-2022"
+        },
+        {
+          custid: 2,
+          name: "Customer 2",
+          amount: 75,
+          transactionDate: "11-21-2022"
+        },
+        {
+          custid: 3,
+          name: "Customer 3",
+          amount: 200,
+          transactionDate: "10-19-2022"
+        },
+        {
+          custid: 3,
+          name: "Customer 3",
+          amount: 224,
+          transactionDate: "11-21-2022"
+        },
+        {
+          custid: 3,
+          name: "Customer 3",
+          amount: 120,
+          transactionDate: "11-29-2022"
+        }
+    ]
+  );
+};
